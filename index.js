a75062b3fbb84881f1d1828746d637b172bd888b
eti kona <etikonapal@gmail.com>
2022-10-25 20:44:39 +0600

    courses data set

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+const courses = require('./data/course.json');
+
+app.get('/', (req, res) => {
+    res.send('welcome to E-study server')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.listen(port, () => {
+    console.log("server is running", port);
+})
